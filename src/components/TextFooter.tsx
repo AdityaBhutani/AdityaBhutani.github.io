77c649593b16d2ecfa808d197a58fee9ce3f4daf
Cendyn_TT0469 <cendyn_tt0469@Cendyn-TT0469s-MacBook-Pro.local>
2026-01-28 04:43:36 +0530

    Made it mine

--- src/components/TextFooter.tsx
+++ src/components/TextFooter.tsx
@@ -1,5 +1,4 @@
 import { Playfair_Display } from "next/font/google";
-import Link from "next/link";
 
 const playfairDisplay = Playfair_Display({
   display: "swap",
@@ -13,7 +12,7 @@ export default function TextFooter() {
       <h1
         className={`absolute left-10 bottom-5 transform -translate-y-1/2 text-white text-5xl font-bold leading-tight ${playfairDisplay.className}`}
       >
-        <span className="text-gray-400">Match</span> <br /> the photo pairs
+        <span className="text-gray-400">Hey Birdie, Match</span> <br /> the photo pairs
       </h1>
 
       {/* Right Text */}
@@ -22,12 +21,6 @@ export default function TextFooter() {
       >
         to reveal <br /> <span className="text-gray-400">the surprise</span>
       </h1>
-
-      {/* Copyright */}
-      <p className="absolute bottom-5 right-5 text-white text-sm opacity-15 hover:opacity-50">
-        © {new Date().getFullYear()}{" "}
-        <Link href="https://visibait.com">visibait.com</Link>
-      </p>
     </>
   );
 }
